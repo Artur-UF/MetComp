e047965e5a8ce3548464e3d2d444a291dde4002d
Artur-UF <artur.uhlik@gmail.com>
2022-07-28 10:06:29 -0300

    Atualização

--- MetCompC/aula7/molas.py
+++ MetCompC/aula7/molas.py
@@ -1,6 +1,9 @@
+'''
+Simulação de n molas acopladas
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
-from random import uniform
 
 
 class Particle:
@@ -70,50 +73,85 @@ class Particle:
             ax.add_patch(circ)
 
 
-def simula(xa, xb, va, vb, r, tf, dt):
-    ma, mb = 1, 2
-    r0 = xa + ((xb - xa)*mb)/(ma + mb)
+def molas_acop(n, r0, xa, xb, va, vb, r, tf, dt):
+    ma, mb = 1, 1
     k = 10
 
     fig, ax = plt.subplots()
 
     f1p = lambda x1, x2: -k * (x1 - r0)
-    fa = lambda x1, x2: k * ((x2 - x1) - r0)
-    fb = lambda x1, x2: -k * ((x2 - x1) - r0)
-    f2p = lambda x1, x2: -k * (x2 - 2 * r0)
+    f12 = lambda x1, x2: k * ((x2 - x1) - r0)
+    f21 = lambda x1, x2: -k * ((x2 - x1) - r0)
+    f2p = lambda x1, x2: -k * (x2 - n * r0)
 
-    pa = Particle(r, [xa, 0], va, fa(xa, xb) + f1p(xa, xb), ma, 'blue')
-    pb = Particle(r, [xb, 0], vb, fb(xa, xb) + f2p(xa, xb), mb, 'red')
-    parts = [pa, pb]
-    fs = [f1p, fa, fb, f2p]
-    for i in range(0, 2, 2):
-        parts[i].vel += ((fs[i](pa.pos[0], pb.pos[0]) + fs[i+1](pa.pos[0], pb.pos[0])) * dt) / 2
+    # Criação das partículas
+    parts = [Particle(r, [xa, 0], va, 0, ma, 'red')]
+    for i in range(1, n):
+        parts.append(Particle(r, [(i + 1) * r0, 0], vb, 0, mb, 'red'))
+
+    # -*- Forças iniciais -*-
+
+    # Paredes
+    parts[0].forc = f1p(parts[0].pos[0], parts[1].pos[0]) + f12(parts[0].pos[0], parts[1].pos[0])
+    parts[-1].forc = f2p(parts[-2].pos[0], parts[-1].pos[0]) + f21(parts[-2].pos[0], parts[-1].pos[0])
+
+    # Meio do sistema
+    for i in range(1, n - 1):
+        parts[i].forc = f21(parts[i-1].pos[0], parts[i].pos[0]) + f12(parts[i].pos[0], parts[i+1].pos[0])
+
+    # -*- Meio passo para a velocidade -*-
+
+    # Paredes
+    parts[0].vel += ((f1p(parts[0].pos[0], parts[1].pos[0]) + f12(parts[0].pos[0], parts[1].pos[0])) * dt) / 2
+    parts[-1].vel += ((f2p(parts[-2].pos[0], parts[-1].pos[0]) + f21(parts[-2].pos[0], parts[-1].pos[0])) * dt) / 2
+
+    # Meio do sistema
+    for i in range(1, n - 1):
+        parts[i].vel += (f21(parts[i-1].pos[0], parts[i].pos[0]) + f12(parts[i].pos[0], parts[i+1].pos[0])) * (dt/2)
     t = np.arange(0, tf, dt)
+
+    # O resto do método
     for ti in t:
         for p in parts:
             p.pos[0] = p.pos[0] + p.vel*dt
-        for i in range(0, 2, 2):
-            parts[i].forc = fs[i](pa.pos[0], pb.pos[0]) + fs[i+1](pa.pos[0], pb.pos[0])
+
+        # -*-Atualizão da força -*-
+
+        # Paredes
+        del parts[0].forc
+        del parts[-1].forc
+        parts[0].forc = (f1p(parts[0].pos[0], parts[1].pos[0]) + f12(parts[0].pos[0], parts[1].pos[0]))
+        parts[-1].forc = (f2p(parts[-2].pos[0], parts[-1].pos[0]) + f21(parts[-2].pos[0], parts[-1].pos[0]))
+
+        # Meio do sistema
+        for i in range(1, n - 1):
+            del parts[i].forc
+            parts[i].forc = f21(parts[i - 1].pos[0], parts[i].pos[0]) + f12(parts[i].pos[0], parts[i + 1].pos[0])
+
         for p in parts:
             p.vel += (p.forc*dt)/2
         Particle.plot(ax)
         plt.grid()
         plt.gca().set_aspect('equal', adjustable='box')
-        plt.xlim(-.5, 8)
+        plt.xlim(-.5, (n+2) * r0)
         plt.ylim(-1, 1)
-        plt.vlines(r0, -.5, .5)
-        plt.vlines(0, -.5, .5)
-        plt.vlines(3 * r0, -.5, .5)
+
+        for i in range(len(parts) + 2):
+            plt.vlines(i * r0, -.5, .5)
+
+        plt.title(f'Molas acopladas\nt = {ti:>5.1f}')
         plt.pause(0.001)
         plt.cla()
 
 
-xa = 1
-xb = 3
-va = .1
-vb = -.1
-r = .3
-tf = 10
+n = 10
+r0 = 1
+xa = 1.3
+xb = 2
+va = 0
+vb = 0
+r = .2
+tf = 20
 dt = .1
-simula(xa, xb, va, vb, r, tf, dt)
 
+molas_acop(n, r0, xa, xb, va, vb, r, tf, dt)
